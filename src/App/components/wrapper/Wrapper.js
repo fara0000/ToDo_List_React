c699f3b4522d7ab73d565ed620edd9aa636ffbf3
fara0000 <fexri_imanzade@mail.ru>
2020-06-02 00:59:13 +0400

    changed main functional component to class

--- src/App/components/wrapper/Wrapper.js
+++ src/App/components/wrapper/Wrapper.js
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { PureComponent } from 'react';
 import { Container } from './styleComponent';
 import { InputWrapper } from './styleComponent';
 import { TaskWrapper } from './styleComponent';
@@ -6,23 +6,29 @@ import ToDoButton from '../button/Button';
 import ToDoInput from '../input/Input';
 import ToDoItem from '../TodoListItem/TodoListItem';
 
-const ToDoContainer = props => {
-    return (
-        <Container>
-            <Container.TitleDiv>
-                <h1>To Do List</h1>
-            </Container.TitleDiv>
-            <Container.MainWrapper>
-                <InputWrapper>
-                    <ToDoInput />
-                    <ToDoButton />
-                </InputWrapper>
-                <TaskWrapper>
-                    <ToDoItem />
-                </TaskWrapper>
-            </Container.MainWrapper>
-        </Container>
-    )
+class ToDoContainer extends React.PureComponent {
+    constructor (props) {
+        super (props)
+    }
+    
+    render () { 
+        return (
+            <Container>
+                <Container.TitleDiv>
+                    <h1>To Do List</h1>
+                </Container.TitleDiv>
+                <Container.MainWrapper>
+                    <InputWrapper>
+                        <ToDoInput />
+                        <ToDoButton />
+                    </InputWrapper>
+                    <TaskWrapper>
+                        <ToDoItem />
+                    </TaskWrapper>
+                </Container.MainWrapper>
+            </Container>
+        )
+    }
 }
 
-export default memo(ToDoContainer);
+export default ToDoContainer;
